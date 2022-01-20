fd6af0206acf1b62304be38df86f4af8b44f03c1
3x3x3 <ivmivm001@gmail.com>
2022-01-20 13:28:00 +0900

    이름은 다르지만 용량이 같은 파일 찾기

---
+++ find_same_size_files.py
@@ -0,0 +1,20 @@
+import os
+
+path_dir = '[FolderPath]'
+file_nms = os.listdir(path_dir)
+file_infos: dict = {}
+
+for file_nm in file_nms:
+    file_size = os.path.getsize(os.path.join(path_dir, file_nm))
+
+    if file_size not in file_infos:
+        file_infos[file_size] = list()
+
+    file_infos[file_size].append(file_nm)
+
+idx = 0
+
+for file_size, file_nms in file_infos.items():
+    if 1 < len(file_nms):
+        print(f'idx: {idx}, size: {round(file_size/1024)}, nms: {file_nms}')
+        idx = idx + 1

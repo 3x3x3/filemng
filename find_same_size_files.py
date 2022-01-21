b3f4a9d70d83561a01894d32f19a97edf6704d11
3x3x3 <ivmivm001@gmail.com>
2022-01-21 12:10:31 +0900

    코드를 main() 함수로 감쌈

--- find_same_size_files.py
+++ find_same_size_files.py
@@ -1,20 +1,27 @@
 import os
 
-path_dir = '[FolderPath]'
-file_nms = os.listdir(path_dir)
-file_infos: dict = {}
 
-for file_nm in file_nms:
-    file_size = os.path.getsize(os.path.join(path_dir, file_nm))
+def main(path: str):
+    file_nms = os.listdir(path)
+    file_infos: dict = {}
 
-    if file_size not in file_infos:
-        file_infos[file_size] = list()
+    for file_nm in file_nms:
+        file_size = os.path.getsize(os.path.join(path, file_nm))
 
-    file_infos[file_size].append(file_nm)
+        if file_size not in file_infos:
+            file_infos[file_size] = list()
 
-idx = 0
+        file_infos[file_size].append(file_nm)
 
-for file_size, file_nms in file_infos.items():
-    if 1 < len(file_nms):
-        print(f'idx: {idx}, size: {round(file_size/1024)}, nms: {file_nms}')
-        idx = idx + 1
+    idx = 0
+
+    for file_size, file_nms in file_infos.items():
+        if 1 < len(file_nms):
+            print(f'idx: {idx}, size: {round(file_size/1024)}, nms: {file_nms}')
+            idx = idx + 1
+
+
+if '__main__' == __name__:
+    path_dir = '[FolderPath]'
+
+    main(path_dir)
